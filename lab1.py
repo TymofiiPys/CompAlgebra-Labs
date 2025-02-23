d907673d701317c4923e9b3e7e2fc21845376e0a
Tymofii Pysarenkov <nfstimoha2004@gmail.com>
2025-02-23 23:31:47 +0200

    finally.

--- lab1.py
+++ lab1.py
@@ -32,7 +32,8 @@ def divisors(n: Union[int, sp.Integer, float]) -> Set[int]:
 
     return divisors
 
-def interpol(xl : List[int], yl : List[int]):
+
+def interpol(xl: List[int], yl: List[int]):
     n = len(xl)
     if n == 0 or n != len(yl) or not len(xl) == len(set(xl)):
         print("Error")
@@ -44,17 +45,17 @@ def interpol(xl : List[int], yl : List[int]):
         for j in range(n):
             if j != i:
                 p = simplify(p * (x - xl[j]) / (xl[i] - xl[j]))
-        
+
         f = simplify(f + p)
 
     return f
 
 
-def kronecker(f : Poly) -> Tuple[int, Poly, bool, Poly, Poly]:
+def kronecker(f: Poly) -> Tuple[int, Poly, bool, Poly, Poly]:
     n = f.degree()
     if n == 0:
         return sp.Abs(f), sp.sign(f), False, None, None
-    
+
     cf, prf = f.primitive()
 
     m = math.floor(n/2)
@@ -67,11 +68,11 @@ def kronecker(f : Poly) -> Tuple[int, Poly, bool, Poly, Poly]:
         yl.append(f.subs(x, xl[i]).evalf())
 
         if yl[i] == 0:
-            return cf, prf, False, x - xl[i], div(prf, x - xl[i])[0]
+            return cf, prf, False, Poly(x - xl[i]), div(prf, x - xl[i])[0]
         dy.append(divisors(yl[i]))
 
     m = list(product(*dy))
-    
+
     for mu in m:
         g = interpol(list(xl), mu)
 
@@ -79,11 +80,73 @@ def kronecker(f : Poly) -> Tuple[int, Poly, bool, Poly, Poly]:
             if LC(g) < 0:
                 g = -g
             return cf, prf, False, g, div(prf, g)[0]
-        
-    return cf, prf, True, None, None
+
+    return cf, prf, True, prf, 1
+
+
+def kroneckerIRR(f: Poly):
+    kr = kronecker(f)
+    if kr[2]:
+        return kr
+    else:
+        cf = kr[0]
+        prf = kr[1]
+        h = kr[3]
+        while not kronecker(h)[2]:
+            h = kronecker(h, x)[3]
+        return cf, prf, False, h, div(prf, h)[0]
+
+
+def kroneckerFACTS(f: Poly):
+    n = f.degree()
+    if n <= 1:
+        return f
+    
+    scal, prf = f.primitive()
+    kr = kroneckerIRR(prf)
+    g, h = kr[3], kr[4]
+    irr, mlt = [g], [1]
+    
+    while not (h == 1):
+        kr = kroneckerIRR(h)
+        g, h = kr[3], kr[4]
+        try:
+            k = irr.index(g)
+            mlt[k] = mlt[k] + 1
+        except ValueError:
+            irr.append(g)
+            mlt.append(1)
+    
+    lng = len(irr)
+    ans = []
+
+    for i in range(lng):
+        ans.append((irr[i], mlt[i]))
+
+    return scal, ans
+
+
 
 if __name__ == "__main__":
-    #print(interpol([0,1,2,3], [-2,4,5,3]))        
-    #print(kronecker(Poly(2*x**5 + x**4 + x**2 +x +2)))
-    print(kronecker(Poly(18*x**5 + 6*x**4 + 12*x**3 + 27*x**2 + 9*x + 18)))
-    #print(kronecker(Poly(2*x**4 + 8)))
+    # print(interpol([0,1,2,3], [-2,4,5,3]))
+    # print(kronecker(Poly(2*x**5 + x**4 + x**2 +x +2)))
+    # print(kronecker(Poly(18*x**5 + 6*x**4 + 12*x**3 + 27*x**2 + 9*x + 18)))
+    #print(kroneckerIRR(Poly(x**7 + x**6 + x**5 + 2*x**4 + 2*x**3 + x**2 + x + 1)))
+    #print(kroneckerFACTS(Poly(x**7 + 2*x**6 + 2*x**5 + 3*x**4 + 3*x**3 + 2*x**2 + 2*x + 1)))
+
+    polynomials = [Poly(14*x**4 - 46*x**3 - 82*x**2 + 138*x + 120),
+                   Poly(x**4 + x**2 - 20)]
+    for polynomial in polynomials:
+        print()
+        print("Поліном:", polynomial)
+        factors = kroneckerFACTS(polynomial)
+        print("Скалярний множник:", factors[0])
+        print("Незвідні множники:")
+        for factor in factors[1]:
+            print(factor[0], ", кратність:", factor[1])
+        product_check = factors[0]
+        for f in factors[1]:
+            product_check = product_check * f[0] ** f[1]
+        print("Перевірка множників: Добуток отриманих множників:", product_check)
+        print("\n===================")
+    # print(kronecker(Poly(2*x**4 + 8)))
